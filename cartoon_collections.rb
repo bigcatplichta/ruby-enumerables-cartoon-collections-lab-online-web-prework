642a11016faddc67079cebccad8cdf6d8f0300b8
Matt Plichta <bigcatplichta@gmail.com>
2019-08-25 19:24:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,14 +12,14 @@ def summon_captain_planet(array)
   array.map { |element| element.capitalize + "!" }
 end
 
-# def long_planeteer_calls(array)
-#   array.each { |word| word.count > 4 }
-# end
-
-def find_the_cheese(array)
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  if array.include?(cheese_types)
-    array.first
+def long_planeteer_calls(array)
+  array.any? { |word| word.count > 4 }
 end
+
+# def find_the_cheese(array)
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+#   if array.include?(cheese_types)
+#     array.first
+# end
