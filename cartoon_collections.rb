c8752d4a7aced3c28a4c9e23c4f82e29b6765009
Matt Plichta <bigcatplichta@gmail.com>
2019-08-25 19:20:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,17 +8,17 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet(array)
-  array.map { |element| element.capitalize + "!" }
-end
+# def summon_captain_planet(array)
+#   array.map { |element| element.capitalize + "!" }
+# end
 
-def long_planeteer_calls(array)
-  array.each { |word| word.count > 4 }
-end
+# def long_planeteer_calls(array)
+#   array.each { |word| word.count > 4 }
+# end
 
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.include
+  array.include?(cheese_types)
 end
