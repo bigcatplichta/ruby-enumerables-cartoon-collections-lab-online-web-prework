f59d74286f227bb0f2ddfa5a83ac184c14fbaf87
Matt Plichta <bigcatplichta@gmail.com>
2019-08-25 20:40:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   array.each do |snack|
-    print snack if cheese_types.include?(snack)
+    return snack if cheese_types.include?(snack)
   end
 
 end
