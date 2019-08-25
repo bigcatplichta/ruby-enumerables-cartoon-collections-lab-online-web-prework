6113a18ce6ca9a850729125923958efe0fb425ab
Matt Plichta <bigcatplichta@gmail.com>
2019-08-25 19:01:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,15 @@
 def roll_call_dwarves(array)
   n = 1
   
-  array.map { |i|
+  array.map do |i|
   name = n.to_s + ". " + i
   puts name
   n += 1 
-  }
-  
+  end
 end
 
 def summon_captain_planet(array)
-  
   array.map { |element| element.capitalize + "!" }
-  
 end
 
 def long_planeteer_calls# code an argument here
