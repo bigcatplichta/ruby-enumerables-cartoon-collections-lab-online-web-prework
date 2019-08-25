1db78be263b4db2a826310db886ebcd37ab3c9c9
Matt Plichta <bigcatplichta@gmail.com>
2019-08-25 18:58:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,10 @@ def roll_call_dwarves(array)
   
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
+  array.map { |element| element.capitalize + "!" }
+  
 end
 
 def long_planeteer_calls# code an argument here
